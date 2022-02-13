866f317759fc881fcf847bde4154536104b2e9e4
Hong An <anhong2017@gmail.com>
2022-02-13 00:36:47 -0600

    Update snp_filter.sh

--- snp_filter.sh
+++ snp_filter.sh
@@ -6,3 +6,8 @@ vcffilter -f "DP > 10 & MQ > 30" a > brapa_outgrpAll_gatk.vcfutils.DP10MQ30.vcf
 bcftools view -v snps -O v brapa_outgrpAll_gatk.vcfutils.DP10MQ30.vcf > brapa_outgrpAll_gatk.vcfutils.DP10MQ30.snp.vcf
 
 rm a
+
+vcftools --vcf brapa_outgrpAll_gatk.vcfutils.DP10MQ30.snp.vcf --plink --out brapa_outgrpAll_gatk.vcfutils.DP10MQ30.snp.plink
+
+sed -i 's/^0\t\(A[0-1][0-9]\)/\1\t\1/g' brapa_outgrpAll_gatk.vcfutils.DP10MQ30.snp.plink.map
+plink --file brapa_outgrpAll_gatk.vcfutils.DP10MQ30.snp.plink --recode vcf --out brapa_outgrpAll_gatk.vcfutils.DP10MQ30.g0.1m0.05.snp --noweb --geno 0.1 --maf 0.05 --allow-extra-chr
