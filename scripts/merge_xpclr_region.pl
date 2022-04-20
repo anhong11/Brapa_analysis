e0ab38842f25e8d605b72e376afbf444267ba391
Hong An <anhong2017@gmail.com>
2022-04-20 15:38:13 -0500

    Update merge_xpclr_region.pl

--- scripts/merge_xpclr_region.pl
+++ scripts/merge_xpclr_region.pl
@@ -17,20 +17,29 @@ while(<IN>){
 		$norm = $norm + $array[12];
 		$num = $num +1;
 		$end = $array[3];
+		if(eof(IN)){
+			my $avg_score = $score/$num;
+			my $avg_norm = $norm/$num;
+			my $size = $end-$start+1;
+                        print OUT "$chr\t$start\t$end\t$size\t$avg_score\t$avg_norm\txpclr\n";
+                }
 		
 	}else{
 		my $avg_score = $score/$num;
 		my $avg_norm = $norm/$num;
 		my $size =$end-$start+1;
-		print OUT "$chr\t$start\t$end\t$size\t$avg_score\t$avg_norm\n";
+		print OUT "$chr\t$start\t$end\t$size\t$avg_score\t$avg_norm\txpclr\n";
 		$chr = $array[1];
 		$start = $array[2];
 		$end = $array[3];
 		$score = $array[11];
 		$norm = $array[12];
 		$num = 1;
+		if(eof(IN)){
+			$size =$end-$start+1;
+			print OUT "$chr\t$start\t$end\t$size\t$score\t$norm\txpclr\n";
+		}
 	}
 }
 
-
-system(`sed -i 's/B.*/CHR\tStart\tEnd\tSize\txpclr\tnorm_xpclr/g' $ARGV[1]`)
+system(`sed -i 's/B.*/CHR\tStart\tEnd\tSize\txpclr\tnorm_xpclr\tsource/g' $ARGV[1]`)
